fe07c097e28c97db1490bacc5c4bb14205fae008
Torsten Curdt <tcurdt@vafer.org>
2025-12-25 23:41:03 +0100

    placeholder

--- Formula/envq.rb
+++ Formula/envq.rb
@@ -2,7 +2,7 @@ class Envq < Formula
   desc "A jq/yq-like tool for .env files"
   homepage "https://github.com/tcurdt/envq"
   url "https://github.com/tcurdt/envq/archive/refs/tags/v0.0.2.tar.gz"
-  sha256 "" # This will be auto-updated by the GitHub action
+  sha256 "0000000000000000000000000000000000000000000000000000000000000000"
   license "Apache-2.0"
 
   depends_on "rust" => :build
